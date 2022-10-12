d2689d345a84d8c0a13d3c92ef6c6552ad50b9ec
Danil <danil-8951@mail.ru>
2022-10-12 19:09:41 +0500

    Создали прогрли программу, которая приветствует по особенному одного человека

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == ("данил"))
+{
+   Console.WriteLine("Ура! Это же Данил!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
